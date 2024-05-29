e71c8266add728fbc05160ee58dca16929734917
kadir572 <kadir9625@gmail.com>
2024-05-29 03:07:26 +0200

    establish local db connection

--- src/main/java/com/kadirk/expensetrackerapi/controller/ExpenseController.java
+++ src/main/java/com/kadirk/expensetrackerapi/controller/ExpenseController.java
@@ -1,7 +1,9 @@
 package com.kadirk.expensetrackerapi.controller;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+// import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,12 +11,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/expenses")
 public class ExpenseController {
 
-  @Value("${spring.application.name}")
-  private String applicationName;
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
+
+  // @Value("${spring.application.name}")
+  // private String applicationName;
 
   @GetMapping
   public String getAll() {
-    System.out.println(applicationName);
-    return new String("All Expenses");
+    try {
+      jdbcTemplate.queryForObject("SELECT 1", Integer.class);
+      return "Database connection is successful";
+    } catch (Exception e) {
+      return "Database connection failed: " + e.getMessage();
+    }
+    // System.out.println(applicationName);
+    // return new String("All Expenses");
   }
 }
